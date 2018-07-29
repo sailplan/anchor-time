4b244b84cc49a411432db640f2138599aa9af690
Brandon Keepers <bkeepers@github.com>
2018-07-29 09:08:42 -0400

    Initial Commit

---
+++ AnchorWatchTests/AnchorWatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  AnchorWatchTests.swift
+//  AnchorWatchTests
+//
+//  Created by Brandon Keepers on 7/29/18.
+//  Copyright © 2018 Brandon Keepers. All rights reserved.
+//
+
+import XCTest
+@testable import AnchorWatch
+
+class AnchorWatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
