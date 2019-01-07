f76922a773c9be442758a9945277f526c2a14f98
Brandon Keepers <brandon@opensoul.org>
2019-01-07 00:23:09 -0600

    Convert distance using current locale

--- AnchorWatch/FormatDisplay.swift
+++ AnchorWatch/FormatDisplay.swift
@@ -1,9 +1,12 @@
 import Foundation
 
 struct FormatDisplay {
-    static func distance(_ meters: Double, unit: UnitLength = .feet) -> String {
+    static let isMetric = Locale.current.usesMetricSystem
+
+    static func distance(_ meters: Double) -> String {
+        let unit: UnitLength = isMetric ? .meters : .feet
         let distance = Measurement<UnitLength>(value: meters, unit: .meters).converted(to: unit)
-        
+
         let formatter = MeasurementFormatter()
         formatter.unitOptions = .providedUnit
         formatter.numberFormatter.maximumFractionDigits = meters <= 3 ? 1 : 0
